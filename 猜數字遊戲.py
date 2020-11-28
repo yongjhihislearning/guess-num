4cd995db26041093a4275265028df28351d3d0b4
Yongjhih Lin <yongjhih85@gmail.com>
2020-11-28 14:11:40 +0800

    final version

--- 猜數字遊戲.py
+++ 猜數字遊戲.py
@@ -4,7 +4,12 @@
 # 猜錯的話 要告訴他 比答案大/小
 
 import random  # 隨機
-r = random.randint(1, 100)
+start = input('請決定隨機數字範圍開始值: ')
+end = input('請決定隨機數字範圍結束值: ')
+start = int(start)
+end = int(end)
+
+r = random.randint(start, end)
 count = 0
 while True:
 	count = count + 1  # 等同於count += 1
@@ -12,7 +17,7 @@ while True:
 	num = int(num)
 	if num == r:
 		print('終於猜對了!')
-		print('這是你猜的第', count, '次')
+		print('這是你猜的第', count, '次') 
 		break
 	elif num > r:
 		print('比答案大')
