9d6e02eb268144c0e85bcdd98ebb6eb6a57d9d5e
Yongjhih Lin <yongjhih85@gmail.com>
2020-11-28 14:07:29 +0800

    add count

--- 猜數字遊戲.py
+++ 猜數字遊戲.py
@@ -5,13 +5,17 @@
 
 import random  # 隨機
 r = random.randint(1, 100)
+count = 0
 while True:
+	count = count + 1  # 等同於count += 1
 	num = input('請猜一個數字: ')
 	num = int(num)
 	if num == r:
 		print('終於猜對了!')
+		print('這是你猜的第', count, '次')
 		break
 	elif num > r:
 		print('比答案大')
 	else:
 		print('比答案小')
+	print('這是你猜的第', count, '次')
